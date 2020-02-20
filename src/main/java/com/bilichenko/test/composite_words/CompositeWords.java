10d820252f2edd6b1ff6775e9e2d3728aa8873c2
Mykhailo Bilichenko <bilichenko.mykhailo@gmail.com>
2020-02-20 21:39:12 +0200

    Merge branch 'master' of /home/iev/workspace3/composite-words with conflicts.

---
+++ src/main/java/com/bilichenko/test/composite_words/CompositeWords.java
@@ -0,0 +1,31 @@
+package com.bilichenko.test.composite_words;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class CompositeWords {
+
+    // this method return only those words,
+    // which cannot be represented as concatenation
+    // of another words presented in input list;
+    public static List<String> retrieveCompositeWords(List<String> sortedWords) {
+        List<String> compositeWords = new ArrayList<>();
+        List<String> strongWords = new ArrayList<>();
+        for (int i = 0; i < sortedWords.size(); i++) {
+            String word = sortedWords.get(i);
+            String temp = word;
+            System.out.println(i + " " + word);
+            for (String strong:strongWords) {
+                temp = temp.replaceAll(strong, "");
+            }
+            if (temp.isEmpty()) {
+                compositeWords.add(word);
+            } else {
+                strongWords.add(temp);
+                strongWords.sort(Comparator.comparing(String::length).reversed());
+            }
+        }
+        return compositeWords;
+    }
+}
